922c5436c68f95560fde86ef536dc821903d409c
Artem Chunikhin <kiryoker@gmail.com>
2018-05-10 03:25:34 +0300

    Before adding test input check

--- Converter/Program.cs
+++ Converter/Program.cs
@@ -6,6 +6,40 @@ using System.Threading.Tasks;
 
 namespace Converter
 {
+    internal class Converter
+    {
+        private readonly Double usd;
+        private readonly Double eur;
+        private readonly Double rub;
+
+        public Converter()
+        { }
+        public Converter(Double usd, Double eur, Double rub)
+        {
+            this.usd = usd;
+            this.eur = eur;
+            this.rub = rub;
+        }
+
+        public Double ConvertValue(Double value)
+        {
+
+
+            return;
+        }
+    }
+
+    internal class InputData
+    {
+
+
+        public void Input()
+        {
+            Console.Write("Enter USD to UAH: ");
+
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
